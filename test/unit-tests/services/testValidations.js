83f5fa9bcfef78ab328adc4269ed323295694268
Jen Sparks <jen.sparks@justice.gov.uk>
2019-08-13 13:21:37 +0100

    change app path 5

--- test/unit-tests/services/testValidations.js
+++ test/unit-tests/services/testValidations.js
@@ -1,5 +1,5 @@
-const { expect } = require('test/util/chai');
-const validations = require('app/services/validations');
+const { expect } = require(__dirname + 'test/util/chai');
+const validations = require(__dirname + 'app/services/validations');
 
 describe('Validations', () => {
   it('exposes express validator for consumption by app.js', () => {
