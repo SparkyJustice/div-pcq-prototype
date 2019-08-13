0f90a77667167bcff017204d262b1c01dfee3ea1
Jen Sparks <jen.sparks@justice.gov.uk>
2019-08-13 13:00:20 +0100

    change app path again

--- app/app.js
+++ app/app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express.Router();
 
 //Services
-const validations = require('app/services/validations');
-const flags = new (require('app/services/flags'))();
+const validations = require(__dirname + 'app/services/validations');
+const flags = new (require(__dirname + 'app/services/flags'))();
 
 //Steps
 const steps = [
-  new (require('app/steps/screening-questions/marriageDate'))(),
-  new (require('app/steps/screening-questions/hasMarriageCert'))(),
-  new (require('app/steps/done'))(),
-  new (require('app/steps/screening-questions/hasRespondentAddress'))(),
-  new (require('app/steps/help-with-fees/hasApplied'))(),
-  new (require('app/steps/genericError'))()
+  new (require(__dirname + 'app/steps/screening-questions/marriageDate'))(),
+  new (require(__dirname + 'app/steps/screening-questions/hasMarriageCert'))(),
+  new (require(__dirname + 'app/steps/done'))(),
+  new (require(__dirname + 'app/steps/screening-questions/hasRespondentAddress'))(),
+  new (require(__dirname + 'app/steps/help-with-fees/hasApplied'))(),
+  new (require(__dirname + 'app/steps/genericError'))()
 ];
 
 //Check Your Answers
-const CheckYourAnswers = require('app/steps/checkYourAnswers');
+const CheckYourAnswers = require(__dirname + 'app/steps/checkYourAnswers');
 const checkYourAnswers = new CheckYourAnswers(steps);
 
 app.use(validations);
